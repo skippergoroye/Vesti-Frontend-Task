0bed0d129cf64933acb5352601d010795e197bdb
skippergoroye <oluwatosinadegoroye21@gmail.com>
2024-05-08 20:42:52 +0100

    feat: react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import AddUserForm from "./components/AddUserForm";
 import UsersList from "./components/UsersList";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={
+      <>
+       <AddUserForm />
+      <UsersList />
+      </>
+      } />
+    </Route>
+  )
+);
+
+
+
 function App() {
   return (
-    <main className="App">
-      <AddUserForm />
-      <UsersList />
-    </main>
+    <RouterProvider router={router} />
   );
 }
+
 
 export default App;
